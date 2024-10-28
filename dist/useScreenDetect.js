e3ecd4d7d231b1ab1a464753c119af5c8ad41455
nomanabid123 <nomanabid3557@gmail.com>
2024-10-28 12:48:45 +0500

    changes

---
+++ dist/useScreenDetect.js
@@ -0,0 +1,89 @@
+"use strict";
+
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireWildcard(require("react"));
+function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
+function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
+function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
+var breakPoints = {
+  xs: 480,
+  sm: 768,
+  md: 1024,
+  lg: 1280,
+  xl: 1536
+};
+var useDetectScreen = function useDetectScreen() {
+  var _useState = (0, _react.useState)({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      breakPoint: 'xl'
+    }),
+    _useState2 = _slicedToArray(_useState, 2),
+    screenSize = _useState2[0],
+    setScreenSize = _useState2[1];
+  var _useState3 = (0, _react.useState)(false),
+    _useState4 = _slicedToArray(_useState3, 2),
+    isMobile = _useState4[0],
+    setIsMobile = _useState4[1];
+  var _useState5 = (0, _react.useState)(false),
+    _useState6 = _slicedToArray(_useState5, 2),
+    isTablet = _useState6[0],
+    setIsTablet = _useState6[1];
+  var _useState7 = (0, _react.useState)(false),
+    _useState8 = _slicedToArray(_useState7, 2),
+    isLaptop = _useState8[0],
+    setIsLaptop = _useState8[1];
+  (0, _react.useEffect)(function () {
+    var getBreakpoint = function getBreakpoint(width) {
+      if (width < breakPoints.xs) return 'xs';
+      if (width < breakPoints.sm) return 'sm';
+      if (width < breakPoints.md) return 'md';
+      if (width < breakPoints.lg) return 'lg';
+      if (width < breakPoints.xl) return 'xl';
+      return 'xxl';
+    };
+    var handleResize = function handleResize() {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        breakpoint: getBreakpoint(window.innerWidth)
+      });
+    };
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return function () {
+      return window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  (0, _react.useEffect)(function () {
+    if (screenSize.breakPoint == 'xs' || screenSize.breakPoint == 'sm') {
+      setIsMobile(true);
+      setIsLaptop(false);
+      setIsTablet(false);
+    }
+    if (screenSize.breakPoint == 'md') {
+      {
+        setIsMobile(false);
+        setIsLaptop(false);
+        setIsTablet(true);
+      }
+    }
+    if (screenSize.breakPoint == 'lg' || screenSize.breakPoint == 'xl' || screenSize.breakPoint == 'xxl') {
+      {
+        setIsMobile(false);
+        setIsLaptop(true);
+        setIsTablet(false);
+      }
+    }
+  }, [screenSize]);
+};
+var _default = exports["default"] = useDetectScreen;
